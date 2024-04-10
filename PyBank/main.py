6cc409b3f0a8f6a79d3f9862a7207edd999e187f
Saad Nasir <nas.saad53@gmail.com>
2024-04-10 12:03:22 -0500

    Removed debugging statements and completed next objective of calculating average change between p/l list.

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,12 +18,11 @@
 #1. Read the CSV Data.
 import csv
 line_num = 0
-file_name = 'C:/Users/saad/Documents/SMU Data Science Bootcamp/Mod 3 PYTHON/python-challenge/PyBank/Resources/budget_data.csv'
+file_name = 'PyBank/Resources/budget_data.csv'
 with open(file_name, 'r') as file:
     raw_data = csv.reader(file)
     data = [line for line in raw_data]
 
-print(data)
 # Separate the dates and the p/l in to their own respectives lists.
 dates = [row[0] for row in data if row[0]!= 'Date']
 profit_and_losses = [int(row[1]) for row in data if row[1]!= 'Profit/Losses']
@@ -39,7 +38,8 @@ total_months = len(dates)
 net_total = sum(profit_and_losses)
 
 #4. The changes in "Profit/Losses" over the entire period, and then the average of those changes
-def get_change(num1,num2):
-    '''Takes 2 numbers, finds the difference and returns the result.
-'''
-    pass
+
+profit_loss_delta = [(profit_and_losses[index+1] - profit_and_losses[index]) for index in range(len(profit_and_losses)) if profit_and_losses[-1] != profit_and_losses[index]]
+
+average_p_and_l = round(sum(profit_loss_delta)/len(profit_loss_delta),2)
+
