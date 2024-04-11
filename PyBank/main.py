b525a29f4ffdb7dc90ea8259b17aac38bf2bc35e
SaadNasir92 <nas.saad53@gmail.com>
2024-04-11 02:13:06 -0500

    Updated comments, cleaned code and spaced as needed.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,49 @@
-#1. Read the CSV Data.
 import csv
+
+# File path declaration
 file_name = 'PyBank/Resources/budget_data.csv'
-with open(file_name, 'r') as file:
-    raw_data = csv.reader(file)
-    data = [line for line in raw_data]
 
-# Separate the dates and the p/l in to their own respectives lists.
-dates = [row[0] for row in data if row[0]!= 'Date']
-profit_and_losses = [int(row[1]) for row in data if row[1]!= 'Profit/Losses']
+# Create function to make dashes in the analysis
+def make_dashes():
+        print('---------------')
+        
+# Create function to make report and print to console.       
+def financial_analysis(csv_data):
+    # Read the CSV Data.
+    with open(csv_data, 'r') as file:
+        raw_data = csv.reader(file)
+        data = [line for line in raw_data]
 
-#2. The total number of months included in the dataset 
+    # Separate the dates and the p/l in to their own respectives lists.
+    dates = [row[0] for row in data if row[0]!= 'Date']
+    profit_and_losses = [int(row[1]) for row in data if row[1]!= 'Profit/Losses']
 
-total_months = len(dates)
+    # Start Printing Analysis
+    print("\nFinancial Analysis\n")
+    make_dashes()
+
+    # Calculate total months
+    total_months = len(dates)
+    print(f'\nTotal Months: {total_months}')
+
+    # The net total amount of "Profit/Losses" over the entire period
+    net_total = sum(profit_and_losses)
+    print(f'\nTotal: ${net_total}')
+
+    # The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    profit_loss_delta = [(profit_and_losses[index+1] - profit_and_losses[index]) for index in range(len(profit_and_losses)) if profit_and_losses[-1] != profit_and_losses[index]]
+    average_p_and_l = round(sum(profit_loss_delta)/len(profit_loss_delta), 2)
+    print(f'\nAverage Change: ${average_p_and_l}')
 
 
+    # The greatest increase in profits (date and amount) over the entire period
+    max_delta = (max(profit_loss_delta))
+    max_delta_mo = dates[profit_loss_delta.index(max_delta) + 1]
+    print(f'\nGreatest Increase in Profits: {max_delta_mo} ({max_delta})')
 
-#3. The net total amount of "Profit/Losses" over the entire period
+    # The greatest decrease in profits (date and amount) over the entire period
+    min_delta = (min(profit_loss_delta))
+    min_delta_mo = dates[profit_loss_delta.index(min_delta) + 1]
+    print(f'\nGreatest Decrease in Profits: {min_delta_mo} ({min_delta})')
 
-net_total = sum(profit_and_losses)
-
-#4. The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-profit_loss_delta = [(profit_and_losses[index+1] - profit_and_losses[index]) for index in range(len(profit_and_losses)) if profit_and_losses[-1] != profit_and_losses[index]]
-
-average_p_and_l = round(sum(profit_loss_delta)/len(profit_loss_delta), 2)
-
-#5. The greatest increase in profits (date and amount) over the entire period
-#debugging line to compare len of delta vector vs the initial vector.
-# print(len(profit_loss_delta))
-max_delta = (max(profit_loss_delta))
-max_delta_mo = dates[profit_loss_delta.index(max_delta) + 1]
-
-
-#6. The greatest decrease in profits (date and amount) over the entire period
-min_delta = (min(profit_loss_delta))
-min_delta_mo = dates[profit_loss_delta.index(min_delta) + 1]
-
+financial_analysis(file_name)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,37 +1,39 @@
 import csv
+
+# File path declaration
+file_name = 'PyPoll/Resources/election_data.csv'
+
 # Create function to make dashes in the analysis
 def make_dashes():
         print('---------------')
 
-# Create function to make report.
-def poll_analysis():
-        #1. Use CSV module to load, read and clean data.
-                # Open file and remove header row.
-        file_name = 'PyPoll/Resources/election_data.csv'
-
+# Create function to make report and print to console.
+def poll_analysis(csv_data):
+        # Read the CSV Data.
         with open(file_name, 'r') as file:
                 raw_data = csv.reader(file)
                 data = [line for line in raw_data]
                 data.pop(0)
 
-                # Make a dictionary that stores keys as candidate names and the total # of votes they earned.
+        # Make a dictionary that stores keys as candidate names and the total # of votes they earned.
         voter_data = {}
         for row in data:
                 if row[2] not in voter_data:
                         voter_data[row[2]] = 1
                 else:
                         voter_data[row[2]] = voter_data[row[2]] + 1
+                        
+        # Begin Printing Analysis Data
         print("\nElection Results\n")
         make_dashes()
+        
         # The total number of votes cast
         total_votes = sum(voter_data.values())
         print(f'\nTotal Votes: {total_votes}\n')
         make_dashes()
 
-
         # A complete list of candidates who received votes
         list_candidates = list(voter_data.keys())
-
 
         # The percentage of votes each candidate won & The total number of votes each candidate won
         winner_tracker = 0
@@ -45,4 +47,5 @@ def poll_analysis():
         make_dashes()
         print(f'\nWinner: {winner}\n')
         make_dashes()
-poll_analysis()
+        
+poll_analysis(file_name)
